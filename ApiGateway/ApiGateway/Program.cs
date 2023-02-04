6599b65462701e964104f10ce9bb6a6ce4abfed5
Dev-khatab <khatap1@hotmail.com>
2023-02-04 12:26:29 -0800

    Adding Docker Container to the Gateway

--- ApiGateway/ApiGateway/Program.cs
+++ ApiGateway/ApiGateway/Program.cs
@@ -3,10 +3,9 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json",optional:false,reloadOnChange:true)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
-
 
 var app = builder.Build();
 await app.UseOcelot();
